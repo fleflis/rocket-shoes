5ff31150ad29979b12f7916b6b56a07819e1d702
fleflis <christian@endless-point.org>
2020-02-27 16:41:40 -0300

    Criação do reducer do carrinho

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyles from './global/styles';
 import Routes from './routes';
 import Header from './components/Header';
+import store from './store';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <BrowserRouter>
         <GlobalStyles />
         <Header />
         <Routes />
       </BrowserRouter>
-    </>
+    </Provider>
   );
 };
 
