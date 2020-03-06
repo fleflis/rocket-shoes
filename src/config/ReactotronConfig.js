dfd0171931d127e9b2aa6e0dd93a824a2e7d641a
fleflis <christian@endless-point.org>
2020-03-06 09:07:18 -0300

    redux saga reactotron config

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotrion-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure()
     .use(reactotronRedux())
+    .use(reactotronSaga())
     .connect();
 
   tron.clear();
