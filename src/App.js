a326fc082d495ce24cc06cbc3b59aef4b1391c4f
fleflis <christian@endless-point.org>
2020-03-06 09:45:27 -0300

    Navegação pelo saga / Final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -8,16 +8,17 @@ import GlobalStyles from './global/styles';
 import Routes from './routes';
 import Header from './components/Header';
 import store from './store';
+import history from './services/history';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <GlobalStyles />
         <Header />
         <Routes />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 };
